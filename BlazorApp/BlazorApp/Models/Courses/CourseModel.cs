e2570827063e964acf68e6015e139ff4fb4f4245
Frida <fridapeersson@gmail.com>
2024-05-22 21:38:04 +0200

    get all courses works, coursedetails aint

--- BlazorApp/BlazorApp/Models/Courses/CourseModel.cs
+++ BlazorApp/BlazorApp/Models/Courses/CourseModel.cs
@@ -1,42 +1,43 @@
-﻿using BlazorProject.Models;
-
-public class CourseModel
+﻿public class CourseModel
 {
-    public string? Id { get; set; }
+    public string Id { get; set; } = null!;
     public string? Title { get; set; }
     public string? Ingress { get; set; }
-    public int? StarRating { get; set; }
     public string? Reviews { get; set; }
-    public string? LikesInNumber { get; set; }
-    public string? LikesInPercent { get; set; }
-    public decimal HoursToComplete { get; set; }
-    public ImageModel? Image { get; set; }
-    public Prices? Prices { get; set; }
+    public string? ImageUri { get; set; }
+    public string? ImageHeaderUri { get; set; }
+    public string[]? Categories { get; set; }
+    public decimal? StarRating { get; set; }
+    public decimal? LikesInNumber { get; set; }
+    public decimal? LikesInPercent { get; set; }
+    public decimal? HoursToComplete { get; set; }
     public bool IsDigital { get; set; }
     public bool IsBestSeller { get; set; }
-    public bool IsSaved { get; set; }
 
 
-    public Category? Category { get; set; }
-    public Author? Author { get; set; }
-    public Content? Content { get; set; }
+    public virtual Prices? Prices { get; set; }
+    public virtual Authors? Authors { get; set; }
+    public virtual Content? Content { get; set; }
+
+    //public virtual List<Categories>? Categories { get; set; }
 }
 
-public class Category
-{
-    public int Id { get; set; }
-    public string? CategoryName { get; set; }
-}
+//public class Categories
+//{
+//    public int Id { get; set; }
+//    public string? CategoryName { get; set; }
+//}
 
-public class Author
+public class Authors
 {
+    public string? AuthorName { get; set; }
+
     //public int Id { get; set; }
     //public string? AuthorTitle { get; set; }
-    public string? AuthorName { get; set; }
-    //public string? AuthorImageUrl { get; set; }
-    //public string? AuthorDescription { get; set; }
-    //public decimal YoutubeSubs { get; set; }
-    //public decimal FacebookSubs { get; set; }
+    //    public string? AuthorImageUrl { get; set; }
+    //    public string? AuthorDescription { get; set; }
+    //    public decimal? YoutubeSubs { get; set; }
+    //    public decimal? FacebookSubs { get; set; }
 }
 
 public class Prices
@@ -50,7 +51,8 @@ public class Content
 {
     public string? Description { get; set; }
     public string[]? Includes { get; set; }
-    public ProgramDetails? ProgramDetails { get; set; }
+    public virtual List<ProgramDetails>? ProgramDetails { get; set; }
+    public string[]? LearnPoints { get; set; }
 }
 
 public class ProgramDetails
